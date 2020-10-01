0d615e0a6d91c97c04053483913fcf1a8818d443
Saad Ahmad Khan (Roll-181111) <71966200+Saad-cr7@users.noreply.github.com>
2020-10-01 16:03:50 +0600

    Add files via upload

---
+++ saad2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+    int i, j, k=1;
+    for (j=1; j<=5; j++)
+    {
+        for (i=1; i<=j; i++)
+            printf ("%d", k++);
+        printf ("\n");
+    }
+    return 0;
+}
